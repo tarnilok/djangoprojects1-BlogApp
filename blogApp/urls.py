1caa209834f20bbdd3f3a8cd2479d07303e7d50a
tarnilok <tarnilok@gmail.com>
2021-11-01 04:06:59 +0300

    home url order changed

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import navbar, user_register, user_login, user_logout, about_page, create_post, user_profile, home_page, user_update, detail_post, delete_post, edit_post
 
 urlpatterns = [
-    path('', home_page, name='home'),
     path('register/', user_register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
@@ -13,4 +12,5 @@ urlpatterns = [
     path('detail/<int:id>', detail_post, name='detail'),
     path('delete/<int:id>', delete_post, name='delete'),
     path('postedit/<int:id>', edit_post, name='postedit'),
+    path('', home_page, name='home'),
 ]
